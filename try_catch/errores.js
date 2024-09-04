8aba683f9d258c126524f2cd08eb60450c639671
emerson <xinayespinoza@gmail.com>
2024-09-03 23:50:57 -0400

    add try catch

---
+++ try_catch/errores.js
@@ -0,0 +1,12 @@
+// cuando todo funcioa okey
+try {
+  console.log(10 / 2)
+} catch (error) {
+  console.error("error detectacdo", error)
+}
+// cuando existe error en el codigo
+try {
+  console.log(hola / hola2)
+} catch (error) {
+  console.error("error detectacdo", error)
+}
